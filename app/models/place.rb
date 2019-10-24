9475a7301ebeb89bd76576d503df014157361ad9
Kathryn Hoelscher <khoelscher0625@gmail.com>
2019-10-24 01:28:43 +0000

    added validations to address and description

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3}
+  validates :address, :description, presence: true
+
+
 end
