516699b0e0697ef76cc835c8e50b728ccad6c040
Kathryn Hoelscher <khoelscher0625@gmail.com>
2019-10-28 21:54:16 +0000

    adjusted code

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,13 +3,13 @@ before_action :authenticate_user!
 
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(comment_params.merge(user: current_user))
+    @place.photos.create(photos_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
 
   private
 
-  def comment_params
+  def photo_params
     params.require(:photo).permit(:caption)
   end
 
