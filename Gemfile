ddfa16e1c5258b0bdbd3c5d738065f51f7c5d2d1
Kathryn Hoelscher <khoelscher0625@gmail.com>
2019-10-30 01:42:32 +0000

    added AWS bucket and modal for photos

--- Gemfile
+++ Gemfile
@@ -83,4 +83,6 @@ gem "figaro"
 
 gem 'carrierwave', '~> 1.0'
 
+gem 'fog-aws'
+
 
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,5 @@
 class Photo < ApplicationRecord
   belongs_to :user
   belongs_to :place
-end
-
-class Photo < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
 end
